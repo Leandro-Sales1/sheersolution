9d5f2a09986fc75761541e483236d1def8485096
Leandro Sales <lsl.2014@hotmail.com>
2024-08-14 19:58:08 -0300

    toggle on FAQ implemented

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Challenge Softtek MVP
 
-1- Limitado pela faculdade (React puro + CSS ).
-2- Projeto mockado
+### 1- Limitado pela faculdade (React puro + CSS ).
+### 2- Projeto mockado
--- src/components/left-side-bar/LeftSideBar.js
+++ src/components/left-side-bar/LeftSideBar.js
@@ -1,7 +1,10 @@
 import './leftSideBar.css'
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 import { faqs } from '../../constants/faqs'
+import { useState } from 'react';
 
 export default function LeftSideBar() {
+  const [isFAQOpen, setIsFAQOpen] = useState(true)
 
   return (
     <>
@@ -10,19 +13,25 @@ export default function LeftSideBar() {
           <img alt="logo" className="logo" src="/assets/logo.svg" />
           <h2 className="company-name">Sheer Solutions</h2>
         </div>
-        <p className="solutions-text">FAQ:</p>
-        <div className="faq-div">
-          {faqs.map((faq, index) => {
-            return (
-              <ul key={index} className='faq-question'>
-                {faq.question}
-                <li key={faq.answer1} className='faq-answer'>{faq.answer1}</li>
-                <li key={faq.answer2} className='faq-answer'>{faq.answer2}</li>
-              </ul>
-            )
-          })}
+        <div className='faq-menu'>
+          <p className="solutions-text">FAQ:</p>
+          <button className="faq-button" onClick={() => setIsFAQOpen(!isFAQOpen)}>
+            {isFAQOpen ? <IoIosArrowDown /> : <IoIosArrowUp />}
+          </button>
         </div>
-        <div className='collaborator-div'>
+        <div className="faq-div">
+          {isFAQOpen &&
+            faqs.map((faq, index) => {
+              return (
+                <ul key={index} className='faq-question'>
+                  {faq.question}
+                  <li key={faq.answer1} className='faq-answer'>{faq.answer1}</li>
+                  <li key={faq.answer2} className='faq-answer'>{faq.answer2}</li>
+                </ul>
+              )
+            })}
+        </div>
+        <div className={`collaborator-div ${!isFAQOpen ? 'faq-closed' : ''}`}>
           <img className='collaborator-img' src='./assets/joaoGomes.png' alt='Imagem do colaborador'></img>
           <h2 className='collaborator-name'>João Gomes</h2>
           <p className='collaborator-title'>Service Desk</p>
