4c32f5e47b505666a0b22c4f8605634d52e3b2b7
Leandro Sales <lsl.2014@hotmail.com>
2024-10-23 11:11:29 -0300

    "Added useRef to Dialogue component and updated useEffect to scroll to bottom of dialogue section on message update; updated dialogue.css to add overflow-y: auto to dialogue-section"

--- src/components/dialogue-area/Dialogue.tsx
+++ src/components/dialogue-area/Dialogue.tsx
@@ -1,11 +1,12 @@
 import './dialogue.css';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import api from '../../api/api';
 import { useRecoilState } from 'recoil';
 import { chatState } from '../../states/chatState';
 
 export default function Dialogue() {
   const [chat, setChat] = useRecoilState(chatState);
+  const dialogueRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -23,8 +24,14 @@ export default function Dialogue() {
     fetchData();
   }, [setChat]);
 
+  useEffect(() => {
+    if (dialogueRef.current) {
+      dialogueRef.current.scrollTop = dialogueRef.current.scrollHeight;
+    }
+  }, [chat.messages]);
+
   return (
-    <section className="dialogue-section">
+    <section className="dialogue-section" ref={dialogueRef}>
       {chat.messages.map((message, index) => (
         <div key={index} className={message.senderType === 'USER' ? 'message-div' : 'response-div'}>
           <p className={message.senderType === 'USER' ? 'label-message' : 'label-response'}>
